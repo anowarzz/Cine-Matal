14517dceb5e60c08715666bdb377a9526c770a8c
anowarzz <anowarhosen444@gmail.com>
2024-01-27 00:13:36 +0600

    sidebar added and movies data added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ const App = () => {
   return (
     <div>
       <Header />
-      <SideBar />
+
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <SideBar />
+        </div>
+      </main>
     </div>
   );
 };
