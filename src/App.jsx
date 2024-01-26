ffe48505f91fa74be67fdbe17a5ec9220953ebe6
anowarzz <anowarhosen444@gmail.com>
2024-01-26 23:31:46 +0600

    project initialized with tailwind and added assets

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <SideBar />
+    </div>
+  );
+};
+
+export default App;
