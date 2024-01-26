d4b1f7a0bcba84d662a9c00ec15f78ca29207abb
anowarzz <anowarhosen444@gmail.com>
2024-01-27 00:31:28 +0600

    created static movie list container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
+import MovieList from "./components/cine/MovieList";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <SideBar />
+          <MovieList />
         </div>
       </main>
     </div>
